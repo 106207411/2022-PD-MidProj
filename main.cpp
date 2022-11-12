fa1e6a507b0ac25f7be5b162031a0850e9443713
andy <106207411@g.nccu.edu.tw>
2022-11-12 23:30:07 +0800

    Fix RE and add leaveRequset

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ void readInput(int** shifts, int** workerDemands, int** leaveDemands, int nK, in
     }
 }
 
-int CalculateBestShiftDemandAndUpdateNaive(int** shifts, int** workerDemands, int* shiftDemand, int nJ, int nK) {
+void CalculateBestShiftDemandAndUpdateNaive(int** shifts, int** workerDemands, int* shiftDemand, int nJ, int nK, int* bestShift) { // 找出最好的班型
     int* shiftWorkerDemand = new int[nK+1];
     for (int i = 0; i <= nK; i++) shiftWorkerDemand[i] = 0;
     
@@ -52,25 +52,53 @@ int CalculateBestShiftDemandAndUpdateNaive(int** shifts, int** workerDemands, in
         for (int z = 0; z < ZONES; z++) {
             sum += workerDemands[j][z];
         }
-        shiftDemand[j] = sum / ZONES;  
-
+        shiftDemand[j] = sum / ZONES;
     }
 
-    int maxIndex = 0;
-    for (int k = 0; k <= nK; k++) {
-        if (shiftWorkerDemand[k] > shiftWorkerDemand[maxIndex]) {
-            maxIndex = k;
+    int* ifnight = new int[nK+1];
+    for (int i = 0; i <= nK; i++) ifnight[i] = 0;
+    for(int i = 0; i <= nK; i++) // 判斷當下班型是否含夜班
+    {
+        for(int j = 18; j <= ZONES; j++)
+        {
+            if(shifts[i][j] == 1)
+            {
+                ifnight[i] = 1;
+                break;
+            }
         }
     }
+
+    // 分早晚班
+    int maxIndex = 0, maxIndex_night = 0;
+    for (int k = 0; k <= nK; k++)
+    {
+        if(ifnight[k] == 1) // 是晚班
+        {
+            if (shiftWorkerDemand[k] > shiftWorkerDemand[maxIndex_night])
+                maxIndex_night = k;
+        }
+        else  // 非晚班
+        {
+            if (shiftWorkerDemand[k] > shiftWorkerDemand[maxIndex])
+                maxIndex = k;
+        }
+
+    }
+
+    bestShift[0] = maxIndex;
+    bestShift[1] = maxIndex_night;
+    
     // release memory
     delete[] shiftWorkerDemand;
 
-    return maxIndex;
+    // return maxIndex;
 }
 
-void arrangeWorkSchedule(int** workSchedule, int** shifts, int** workerDemands, int** leaveDemands, int nI, int nJ, int nK, int L, int R) {
+
+void arrangeWorkSchedule(int** workSchedule, int** shifts, int** workerDemands, int** leaveDemands, int nI, int nJ, int nK, int L, int R, int* bestShift, int w1, int w2) {
     // initialize workSchedule to -1
-    for (int i = 0; i < nI; i++) {
+    for (int i = 0; i < nI; i++) { // 員工*天數
         workSchedule[i] = new int[nJ];
         for (int j = 0; j < nJ; j++) {
             workSchedule[i][j] = -1;
@@ -80,15 +108,16 @@ void arrangeWorkSchedule(int** workSchedule, int** shifts, int** workerDemands, 
     // Average demand of each day
     int* shiftDemand = new int[nJ];
     for (int j = 0; j < nJ; j++) shiftDemand[j] = 0;
-    int bestShiftIndex = CalculateBestShiftDemandAndUpdateNaive(shifts, workerDemands, shiftDemand, nJ, nK);
+
+    CalculateBestShiftDemandAndUpdateNaive(shifts, workerDemands, shiftDemand, nJ, nK, bestShift); // 找出最好的排班
 
     // workdays of each worker
     int* workdays = new int[nI];
-    for (int i = 0; i < nJ; i++) workdays[i] = 0;
-
-    for (int j = 0; j < nJ; j++) {
-        int demand = shiftDemand[j];
-        for (int i = 0; i < nI; i++) {
+    for (int i = 0; i < nI; i++) workdays[i] = 0; 
+    
+    for (int j = 0; j < nJ; j++) { // 天數
+        int demand = shiftDemand[j]; // shiftdemand 每天平均需求
+        for (int i = 0; i < nI; i++) {   // 員工數
             if (demand > 0) {
                 // if the worker has no workdays left, day off
                 if (workdays[i] == nJ - L) {
@@ -113,25 +142,26 @@ void arrangeWorkSchedule(int** workSchedule, int** shifts, int** workerDemands, 
                 // if the worker hasn't been assigned the shift
                 if (workSchedule[i][j] == -1) {
                     // check if the worker has a leave demand on that day
-                    bool hasLeaveDemand = false;
-                    for (int r = 0; r < R; r++) {
-                        if (leaveDemands[0][r] == i+1 && leaveDemands[1][r] == j+1) {
-                            hasLeaveDemand = true;
-                            break;
-                        }
-                    }
-                    if (hasLeaveDemand) {
-                        workSchedule[i][j] = 0;
-                    }
-                    else {
-                        workSchedule[i][j] = bestShiftIndex;
-                        demand--;
-                        workdays[i]++;
-                    }
-                    // // TA-naiive
-                    // workSchedule[i][j] = bestShiftIndex;
-                    // demand--;
-                    // workdays[i]++;
+                    // bool hasLeaveDemand = false;
+                    // for (int r = 0; r < R; r++) {
+                    //     if (leaveDemands[0][r] == i+1 && leaveDemands[1][r] == j+1) { // i員工在第j天要休假
+                    //         hasLeaveDemand = true;
+                    //         break;
+                    //     }
+                    // }
+                    // if (hasLeaveDemand) {
+                    //     workSchedule[i][j] = 0;
+                    // }
+
+                    // else {
+                    //     workSchedule[i][j] = bestShift[0];
+                    //     demand--; 
+                    //     workdays[i]++;
+                    // }
+                    // TA-naiive
+                    workSchedule[i][j] = bestShift[0];
+                    demand--;
+                    workdays[i]++;
                 }
             }
             else {
@@ -139,9 +169,63 @@ void arrangeWorkSchedule(int** workSchedule, int** shifts, int** workerDemands, 
             }
         }
     }
+    
+    // 缺工數 (第i天第z個時段)
+    int** lackPerZone = new int*[nJ];
+    for (int i = 0; i < nJ; i++) {
+        lackPerZone[i] = new int[ZONES];
+        for (int z = 0; z <= ZONES; z++) {
+            lackPerZone[i][z] = 0;
+        }
+    }
+
+    // 計算缺工數
+    for (int day = 0; day < nJ; day++) {
+        int workersPerDay[ZONES] = {0};
+        for (int worker = 0; worker < nI; worker++) {
+            for (int h = 0; h < ZONES; h++) 
+                workersPerDay[h] += shifts[workSchedule[worker][day]][h];
+        }
+        for (int h = 0; h < ZONES; h++) 
+            lackPerZone[day][h]= workerDemands[day][h] - workersPerDay[h];
+    }
+
+    
+    for (int r = 0; r < R; r++) // i員工在第j天要休假
+    {
+        int counts = 0;
+        int i = leaveDemands[0][r] - 1, j = leaveDemands[1][r] - 1; // i 員工編號 j 天數
+        if (workSchedule[i][j] != 0) // 該天有上班，但要請假
+        {
+            for (int h = 0; h < ZONES; h++)
+            {
+                // 該員工在該時段要上班，請假使得該時端缺工
+                if(lackPerZone[j][h] >= 0 && shifts[workSchedule[i][j]][h] == 1)
+                    counts++;
+                
+            }
+            // 請假後的目標函數較小，則請假
+            if(w1 > counts) {
+                workSchedule[i][j] = 0;
+                // 更新每天每個時段的缺工數
+                for (int h = 0; h < ZONES; h++)
+                {
+                    if(shifts[workSchedule[i][j]][h] == 1)
+                        lackPerZone[j][h]++;
+                }
+            }
+        }
+    }
+
+    
+    // cout << bestShift[0] << "," << bestShift[1];
     // release memory
     delete[] shiftDemand;
     delete[] workdays;
+    for (int i = 0; i < nJ; i++) {
+        delete[] lackPerZone[i];
+    }
+    delete[] lackPerZone;
 }
 
 
@@ -152,10 +236,10 @@ int main() {
     int** workerDemands = new int*[nJ];
     int** leaveDemands = new int*[2];
     readInput(shifts, workerDemands, leaveDemands, nK, nJ, R);
-    
+    int bestShift[2] = {0};
     // workSchedule[i][j] means the shift of worker i on day j
     int** workSchedule = new int*[nI];
-    arrangeWorkSchedule(workSchedule, shifts, workerDemands, leaveDemands, nI, nJ, nK, L, R);
+    arrangeWorkSchedule(workSchedule, shifts, workerDemands, leaveDemands, nI, nJ, nK, L, R, bestShift, w1, w2);
 
     // print workSchedule
     for (int i = 0; i < nI; i++) {
@@ -165,19 +249,6 @@ int main() {
         }
         cout << endl;
     }
-
-    // // calculate the lack of workers
-    // int lack = 0;
-    // for (int day = 0; day < nJ; day++) {
-    //     int workersPerDay[ZONES] = {0};
-    //     for (int worker = 0; worker < nI; worker++) {
-    //         for (int h = 0; h < ZONES; h++) 
-    //             workersPerDay[h] += shifts[workSchedule[worker][day]][h];
-    //     }
-    //     for (int h = 0; h < ZONES; h++) 
-    //         lack += max(0, workerDemands[day][h] - workersPerDay[h]);
-    // }
-    // cout << "lack: " << lack << endl;
 
     // release memory
     for (int i = 0; i < nK+1; i++) delete[] shifts[i];
